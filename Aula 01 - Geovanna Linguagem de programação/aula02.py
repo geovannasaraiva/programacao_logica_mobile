644c0382f14b04174055961d3bfb5788e24c9d2c
Geovanna Saraiva <166664913+geovannasaraiva@users.noreply.github.com>
2024-04-16 19:05:06 -0300

    Upload Python
    Aula Python

---
+++ Aula 01 - Geovanna Linguagem de programação/aula02.py
@@ -0,0 +1,10 @@
+#print("ola mundo, finalmente quebrei a maldição")
+
+#um comentario 
+
+print("Olá bem vindo ao nosso app!")
+print("Seu nome é David, sua idade é 25 anos. Gosta de tocar bateria na igreja!")
+nome  = input("Qual é seu nome?:")
+idade =  input("Qual é a sua idade?")
+gosto = input("Qual é o seu hobbie?")
+print(f"Olá {nome}, você tem {idade}, anos")
